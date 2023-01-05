7d2c16ef159107b6e6131d32a6f49aa1e5291810
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-01-05 16:08:53 +0900

    init wheere-renewal-back

---
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/WheereApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.sjjpl138.wheere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheereApplication.class, args);
+	}
+
+}
---
+++ wheere-renewal-back/wheere/src/test/java/kr/ac/kumoh/sjjpl138/wheere/WheereApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.sjjpl138.wheere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
