1acc7667ea045d854e01a2ac834f28b337d20229
parkjunsik <akfldkskgorn@gmail.com>
2023-01-07 23:26:49 +0900

    style: custom_radio 정의

---
+++ wheere-renewal-front/wheere/lib/view/common/custom_radio/custom_radio_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:wheere/styles/colors.dart';
+
+class CustomRadioButton<T> extends StatefulWidget {
+  final T? value;
+  final T? groupValue;
+
+  final void Function(T?)? onChanged;
+
+  const CustomRadioButton({
+    Key? key,
+    required this.value,
+    required this.groupValue,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  State<CustomRadioButton> createState() => _CustomRadioButtonState();
+}
+
+class _CustomRadioButtonState extends State<CustomRadioButton> {
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      onPressed: () =>
+          widget.onChanged != null ? widget.onChanged!(widget.value) : null,
+      style: OutlinedButton.styleFrom(
+        backgroundColor: widget.value == widget.groupValue
+            ? CustomColor.itemSubColor
+            : CustomColor.itemDisabledColor,
+        shape: const CircleBorder(),
+        side: BorderSide.none,
+      ),
+      child: const Icon(
+        Icons.check,
+        color: CustomColor.textReverseColor,
+        size: 35.0,
+      ),
+    );
+  }
+}
---
+++ wheere-renewal-front/wheere/lib/view/common/custom_radio/custom_radio_list_tile.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:wheere/styles/colors.dart';
+import 'package:wheere/view/common/custom_radio/custom_radio_button.dart';
+
+class CustomRadioListTitle<T> extends StatelessWidget {
+  final T? value;
+  final T? groupValue;
+
+  final String title;
+
+  final void Function(T?)? onChanged;
+
+  const CustomRadioListTitle(
+      {Key? key,
+      required this.value,
+      required this.groupValue,
+      required this.title,
+      required this.onChanged})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: const Size.fromHeight(50).height,
+      decoration: BoxDecoration(
+          color: CustomColor.textFormMainColor,
+          borderRadius: BorderRadius.circular(9)),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              title,
+              style: const TextStyle(
+                color: CustomColor.textMainColor,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const Spacer(),
+          CustomRadioButton<T>(value: value, groupValue: groupValue, onChanged: onChanged),
+        ],
+      ),
+    );
+  }
+}
