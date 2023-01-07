c0207054e8b4e9f32f414068ff3245672ad12715
parkjunsik <akfldkskgorn@gmail.com>
2023-01-07 20:26:38 +0900

    style: custom_outlined_button 정의

---
+++ wheere-renewal-front/wheere/lib/view/common/custom_outlined_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../../styles/colors.dart';
+
+class CustomOutlinedButton extends StatelessWidget {
+  final void Function()? onPressed;
+
+  final String text;
+
+  const CustomOutlinedButton(
+      {Key? key, required this.onPressed, required this.text})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      onPressed: onPressed,
+      style: OutlinedButton.styleFrom(
+          backgroundColor: CustomColor.itemSubColor,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          minimumSize: const Size.fromHeight(50),
+          side: BorderSide.none),
+      child: Text(
+        text,
+        style: const TextStyle(
+            color: CustomColor.textReverseColor,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
