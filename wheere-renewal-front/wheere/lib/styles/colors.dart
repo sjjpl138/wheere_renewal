0f2c0ea06c7dc9252c896b54df9136b123313832
parkjunsik <akfldkskgorn@gmail.com>
2023-01-07 16:56:27 +0900

    style: colors 정의

---
+++ wheere-renewal-front/wheere/lib/styles/colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class CustomColor {
+  static const Color itemMainColor = Color(0xff161853);
+  static const Color itemSubColor = Color(0xff292c6d);
+  static const Color itemDisabledColor = Color(0xfdD9d9d9);
+  static const Color textMainColor = Color(0xff292c6d);
+  static const Color textSubColor = Color(0xff565886);
+  static const Color textReverseColor = Color(0xffffffff);
+  static const Color backgroundMainColor = Color(0xfffdfaf7);
+  static const Color textFormMainColor = Color(0xffffffff);
+  static const Color pointColor = Color(0xffec255a);
+}
