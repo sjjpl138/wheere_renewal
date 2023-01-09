f48216f007bc38a1ea9bf4cef378666eb7606fec
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-01-09 22:08:54 +0900

    feat: create Seat domain

---
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/entity/Seat.java
@@ -0,0 +1,29 @@
+package kr.ac.kumoh.sjjpl138.wheere.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Seat {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "SEAT_ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ROUTE_ID")
+    private Route route;
+
+    private int totalSeatsNum;     // 예약 가능한 총 좌석 수
+
+    private int leftSeatsNum;     // 예약 가능한 남은 좌석 수
+
+    private LocalDate reservationDate;
+}
