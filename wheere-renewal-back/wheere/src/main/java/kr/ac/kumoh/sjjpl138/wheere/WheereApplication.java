96474731af6007662955d609a6d963e563571052
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-01-09 16:22:13 +0900

    feat: BaseTimeEntity 생성
    생성 시간, 최종 수정 시간 필드 생성
    등록자와 수정자는 추후 추가 예정

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/WheereApplication.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/WheereApplication.java
@@ -2,12 +2,18 @@ package kr.ac.kumoh.sjjpl138.wheere;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class WheereApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WheereApplication.class, args);
 	}
-
 }
