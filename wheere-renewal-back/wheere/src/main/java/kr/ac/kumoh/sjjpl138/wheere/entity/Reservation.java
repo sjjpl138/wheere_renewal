5168bb07124e3af8b9d69fbe5997e97782ab69a9
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-01-09 22:08:14 +0900

    Reservation 도메인 생성

---
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/entity/Reservation.java
@@ -0,0 +1,38 @@
+package kr.ac.kumoh.sjjpl138.wheere.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reservation extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "RESERVATION_ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "BUS_ID")
+    private Bus bus;
+
+    @Enumerated(EnumType.STRING)
+    private ReservationState reservationState;
+
+    private int busCount; // 탑승해야하는 버스 개수 (환승 구분)
+
+    private String startStation;
+
+    private String endStation;
+
+    private LocalDate reservationDate;
+}
