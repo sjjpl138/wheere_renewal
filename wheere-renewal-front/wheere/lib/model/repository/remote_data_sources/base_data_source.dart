924c0ab7765f1b992b3cd2f887a94ac5654f93e8
parkjunsik <akfldkskgorn@gmail.com>
2023-01-10 15:43:00 +0900

    feature: login_data_source 구현

---
+++ wheere-renewal-front/wheere/lib/model/repository/remote_data_sources/base_data_source.dart
@@ -0,0 +1,71 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+abstract class BaseDataSource {
+  static const _baseUrl = "";
+  static const _headers = {"Accept": "application/json"};
+
+  static Future<Map<String, dynamic>?> get(String path) async {
+    try {
+      final res = await http.get(Uri.parse(_baseUrl + path), headers: _headers);
+      switch (res.statusCode) {
+        case 200:
+          return json.decode(utf8.decode(res.bodyBytes));
+        default:
+          return null;
+      }
+    } catch (e) {
+      return null;
+    }
+  }
+
+  static Future<Map<String, dynamic>?> post(
+      String path, Map<String, dynamic> body) async {
+    try {
+      final res = await http.post(Uri.parse(_baseUrl + path),
+          headers: _headers, body: jsonEncode(body));
+      switch (res.statusCode) {
+        case 200:
+          return json.decode(utf8.decode(res.bodyBytes));
+        default:
+          return null;
+      }
+    } catch (e) {
+      return null;
+    }
+  }
+
+  static Future<Map<String, dynamic>?> put(
+      String path, Map<String, dynamic> body) async {
+    try {
+      final res = await http.put(Uri.parse(_baseUrl + path),
+          headers: _headers, body: body);
+      switch (res.statusCode) {
+        case 200:
+          return json.decode(utf8.decode(res.bodyBytes));
+        default:
+          return null;
+      }
+    } catch (e) {
+      return null;
+    }
+  }
+
+  static Future<Map<String, dynamic>?> delete(
+      String path, Map<String, dynamic> body) async {
+    try {
+      final res = await http.delete(Uri.parse(_baseUrl + path),
+          headers: _headers, body: body);
+      switch (res.statusCode) {
+        case 200:
+          return json.decode(utf8.decode(res.bodyBytes));
+        default:
+          return null;
+      }
+    } catch (e) {
+      return null;
+    }
+  }
+
+  String path = "";
+}
