ede97ec25248ff814a6bba20eb7e0aefef46cae4
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-01-13 00:22:28 +0900

    feat(server): BusRepository 생성

---
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/bus/repository/BusRepository.java
@@ -0,0 +1,7 @@
+package kr.ac.kumoh.sjjpl138.wheere.bus.repository;
+
+import kr.ac.kumoh.sjjpl138.wheere.bus.Bus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BusRepository extends JpaRepository<Bus, Long> {
+}
