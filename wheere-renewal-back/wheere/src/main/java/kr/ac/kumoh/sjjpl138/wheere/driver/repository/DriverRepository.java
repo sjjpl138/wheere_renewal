6a9bdbf811a7d8bf41311eb64cb4e57f5fde30ff
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-01-13 00:02:02 +0900

    Revert "feat(server): DriverRepository 생성"
    This reverts commit f7cc4d512ceadb4f2a7541ed78cd0ddf39e27571.

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/driver/repository/DriverRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.ac.kumoh.sjjpl138.wheere.driver.repository;
-
-import kr.ac.kumoh.sjjpl138.wheere.driver.Driver;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DriverRepository extends JpaRepository<Driver, String> {
-}
