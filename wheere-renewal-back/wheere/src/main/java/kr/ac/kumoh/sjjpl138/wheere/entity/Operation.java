6a9a512c58680c0842123165e809d8a27b1c3e46
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-01-12 19:48:26 +0900

    feat: Operation 도메인

---
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/entity/Operation.java
@@ -0,0 +1,29 @@
+package kr.ac.kumoh.sjjpl138.wheere.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Operation extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "BUS_DRIVER_ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "BUS_ID")
+    private Bus bus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "DRIVER_ID")
+    private Driver driver;
+
+    private LocalDate operationDate;
+
+    private BusDriverStatus status; // 버스 배정 상태
+}
