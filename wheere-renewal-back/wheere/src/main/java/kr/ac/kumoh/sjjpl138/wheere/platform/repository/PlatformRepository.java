1d996838d49876c016793cf65db15bfbef8ab3cb
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-01-12 23:44:15 +0900

    feat(server): PlatformRepository 생성

---
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/platform/repository/PlatformRepository.java
@@ -0,0 +1,7 @@
+package kr.ac.kumoh.sjjpl138.wheere.platform.repository;
+
+import kr.ac.kumoh.sjjpl138.wheere.platform.Platform;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlatformRepository extends JpaRepository<Platform, Long> {
+}
