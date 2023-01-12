d0afe93d0abb210805b401ac3fdc213f346e8542
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-01-13 00:01:49 +0900

    Revert "feat(server): BusRepository 생성"
    This reverts commit 6aed1193b785f688a1a72d53d2fba59e450919aa.

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/bus/repository/BusRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.ac.kumoh.sjjpl138.wheere.bus.repository;
-
-import kr.ac.kumoh.sjjpl138.wheere.bus.Bus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BusRepository extends JpaRepository<Bus, Long> {
-}
