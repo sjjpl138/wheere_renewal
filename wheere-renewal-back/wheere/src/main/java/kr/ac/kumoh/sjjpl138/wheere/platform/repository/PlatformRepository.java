4b8ea5e1bb75cb30ae2231b54217533b2f72ccda
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-01-13 00:02:30 +0900

    Revert "feat(server): PlatformRepository 생성"
    This reverts commit 1d996838d49876c016793cf65db15bfbef8ab3cb.

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/platform/repository/PlatformRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.ac.kumoh.sjjpl138.wheere.platform.repository;
-
-import kr.ac.kumoh.sjjpl138.wheere.platform.Platform;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlatformRepository extends JpaRepository<Platform, Long> {
-}
