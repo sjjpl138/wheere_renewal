14c007bae52f36fefcc64586a93900b6d32cbd6a
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-01-13 00:02:11 +0900

    Revert "feat(server): ReservationRepository 생성"
    This reverts commit 764ce2d622efe1f708c950a7b7d91a73184e7e94.

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/reservation/repository/ReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.ac.kumoh.sjjpl138.wheere.reservation.repository;
-
-import kr.ac.kumoh.sjjpl138.wheere.reservation.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-}
