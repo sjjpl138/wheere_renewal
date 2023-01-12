9db92b5bc8a0fbd7a6f513979f2af7a551d37bfd
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-01-13 00:02:03 +0900

    Revert "feat(server): MemberRepository 생성"
    This reverts commit af344c63c229bc7376c5ed07e022ef76abe87d44.

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/member/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package kr.ac.kumoh.sjjpl138.wheere.member.repository;
-
-import kr.ac.kumoh.sjjpl138.wheere.member.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, String> {
-
-}
