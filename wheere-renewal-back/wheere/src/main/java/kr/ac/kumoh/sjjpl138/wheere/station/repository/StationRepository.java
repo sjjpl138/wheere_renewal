3396a7115729f243ff9aafd7210f1f77254f70c3
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-01-13 16:47:37 +0900

    rename(server): SeatRepository -> StationRepository

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/station/repository/StationRepository.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/station/repository/StationRepository.java
@@ -1,7 +1,8 @@
 package kr.ac.kumoh.sjjpl138.wheere.station.repository;
 
 import kr.ac.kumoh.sjjpl138.wheere.seat.Seat;
+import kr.ac.kumoh.sjjpl138.wheere.station.Station;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SeatRepository extends JpaRepository<Seat, Long> {
+public interface StationRepository extends JpaRepository<Station, Long> {
 }
