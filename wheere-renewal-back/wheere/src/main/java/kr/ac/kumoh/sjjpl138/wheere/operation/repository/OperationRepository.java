00ae4a3efe23fcabd7eee5636584d48e859646e7
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-01-18 19:43:45 +0900

    remove(server): OperationRepository 제거

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/operation/repository/OperationRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.ac.kumoh.sjjpl138.wheere.operation.repository;
-
-import kr.ac.kumoh.sjjpl138.wheere.operation.Operation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OperationRepository extends JpaRepository<Operation, Long> {
-}
