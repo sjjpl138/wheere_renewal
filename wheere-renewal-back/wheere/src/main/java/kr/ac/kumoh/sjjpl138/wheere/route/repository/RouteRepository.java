7cc67701ad1f0927eaa80ceb5332795e521fe1ae
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-01-18 17:47:32 +0900

    remove(server): RouteRepository 제거

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/route/repository/RouteRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.ac.kumoh.sjjpl138.wheere.route.repository;
-
-import kr.ac.kumoh.sjjpl138.wheere.route.Route;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RouteRepository extends JpaRepository<Route, Long> {
-}
