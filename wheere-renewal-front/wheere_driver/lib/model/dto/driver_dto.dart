75dbfabdd975265cf8cc3c41f612aba7353e8386
Journeybongbong <77794756+Journeybongbong@users.noreply.github.com>
2023-01-19 00:22:18 +0900

    feature: (driver) DriverDTO 추가

---
+++ wheere-renewal-front/wheere_driver/lib/model/dto/driver_dto.dart
@@ -0,0 +1,40 @@
+import 'package:wheere_driver/model/dto/dtos.dart';
+
+class DriverDTO {
+  String dName;
+  int bId;
+  String vNo;
+  String routeId;
+  String bNo;
+  List<StationDTO> route;
+  List<ReservationDTO> reservations;
+
+  DriverDTO({
+    required this.dName,
+    required this.bId,
+    required this.vNo,
+    required this.routeId,
+    required this.bNo,
+    required this.route,
+    required this.reservations
+  });
+
+  factory DriverDTO.fromJson(Map<String, dynamic> json) {
+
+    var getRouteList = json['reservations'] as List;
+    List<StationDTO> routes = getRouteList.map((i) => StationDTO.fromJson(i)).toList();
+
+    var getReservationList = json['reservations'] as List;
+    List<ReservationDTO> reservationList = getReservationList.map((i) => ReservationDTO.fromJson(i)).toList();
+    return DriverDTO(
+      dName: json["dName"],
+      bId: json["bId"],
+      vNo: json["vNo"],
+      routeId: json["routeId"],
+      bNo: json["bNo"],
+      route: routes,
+      reservations: reservationList,
+    );
+  }
+
+}
