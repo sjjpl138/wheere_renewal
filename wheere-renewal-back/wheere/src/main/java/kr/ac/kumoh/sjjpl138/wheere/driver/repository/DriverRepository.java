9bacf2f4514d4dfece72060ed860c5f230af324a
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-01-19 17:30:55 +0900

    feat(server): DriverRepository findByBusId 메서드 생성

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/driver/repository/DriverRepository.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/driver/repository/DriverRepository.java
@@ -2,6 +2,12 @@ package kr.ac.kumoh.sjjpl138.wheere.driver.repository;
 
 import kr.ac.kumoh.sjjpl138.wheere.driver.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface DriverRepository extends JpaRepository<Driver, String> {
+    Driver findDriverById(String driverId);
+
+    @Query("select d from Driver d join d.bus b on b.id = :busId")
+    Driver findByBusId(@Param("busId")Long busId);
 }
