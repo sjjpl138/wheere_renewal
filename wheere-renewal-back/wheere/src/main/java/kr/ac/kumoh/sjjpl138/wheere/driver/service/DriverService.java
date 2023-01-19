221be39e36e1aad85529753a8eb0bc8347ef9f0e
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-01-19 17:23:32 +0900

    feat(server): DriverService 버스기사 버스 변경 기능 추가

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/driver/service/DriverService.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/driver/service/DriverService.java
@@ -1,12 +1,18 @@
 package kr.ac.kumoh.sjjpl138.wheere.driver.service;
 
+import kr.ac.kumoh.sjjpl138.wheere.bus.Bus;
+import kr.ac.kumoh.sjjpl138.wheere.bus.repository.BusRepository;
 import kr.ac.kumoh.sjjpl138.wheere.driver.Driver;
 import kr.ac.kumoh.sjjpl138.wheere.driver.dto.DriverLogInRequestDto;
 import kr.ac.kumoh.sjjpl138.wheere.driver.dto.DriverLoginResponseDto;
 import kr.ac.kumoh.sjjpl138.wheere.driver.repository.DriverRepository;
+import kr.ac.kumoh.sjjpl138.wheere.platform.repository.PlatformRepository;
+import kr.ac.kumoh.sjjpl138.wheere.reservation.repository.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @Service
 @Transactional(readOnly = true)
@@ -14,6 +20,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class DriverService {
 
     private final DriverRepository driverRepository;
+    private final BusRepository busRepository;
+    private final PlatformRepository platformRepository;
 
     /**
      * 버스 기사 로그인
@@ -28,14 +36,16 @@ public class DriverService {
     /**
      * 버스 기사 정보 수정 (버스 변경)
      * 반환값으로 배정된 버스 정보? 등등
+     * @param driverId
+     * @param vehicleNo
+     * @return
      */
     @Transactional
-    public void changeBus() {
+    public Bus changeBus(String driverId, String vehicleNo) {
+        Driver findDriver = driverRepository.findDriverById(driverId);
+        Bus findBus = busRepository.findBusByVehicleNo(vehicleNo);
+        findDriver.assignBus(findBus);
 
-    }
-
-    @Transactional
-    public void delete(Driver driver) {
-
+        return findBus;
     }
 }
