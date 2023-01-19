0fbee4179a6522cdf4649e07298735ad5ce0e850
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-01-19 17:29:08 +0900

    refact(server): checkRoutes 리팩토링

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/member/service/MemberService.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/member/service/MemberService.java
@@ -13,7 +13,6 @@ import kr.ac.kumoh.sjjpl138.wheere.member.Member;
 import kr.ac.kumoh.sjjpl138.wheere.member.dto.MemberDto;
 import kr.ac.kumoh.sjjpl138.wheere.member.dto.RetrieveRoutesRequest;
 import kr.ac.kumoh.sjjpl138.wheere.member.repository.MemberRepository;
-import kr.ac.kumoh.sjjpl138.wheere.operation.repository.OperationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -43,7 +42,6 @@ public class MemberService {
     @Value("${odsay-key}")
     private String apiKey;
     private final MemberRepository memberRepository;
-    private final OperationRepository operationRepository;
     private final DriverRepository driverRepository;
 
     /**
@@ -139,14 +137,11 @@ public class MemberService {
 
         List<Course> courses = new ArrayList<>();
 
-        // ============ json 파싱 ================ //
-        JSONObject jsonObj = new JSONObject(jsonResult);
-        JSONObject rootJsonObject = jsonObj.getJSONObject("result");
+        /* json parsing */
+        JSONObject rootJsonObject = getJsonObject(jsonResult);
 
         // 값이 0 (도시내) 인 경우에만 응답하도록
-        // 값이 1인 경우 빈 리스트 반환
-        int searchType = rootJsonObject.getInt("searchType");
-        log.debug("searchType = {}", searchType);
+        int searchType = getSearchType(rootJsonObject);
 
         // 조회한 범위가 도시 내가 아닌 경우 empty Optional 반환
         if (isNotInCity(searchType)) {
@@ -155,15 +150,42 @@ public class MemberService {
 
         // 도시간 '직통' 탐색 결과 유무
         // 0: 직통, 1: 직통 없음
-        int outTrafficCheck = rootJsonObject.getInt("outTrafficCheck");
-        log.debug("outTrafficCheck = {}", outTrafficCheck);
+        int outTrafficCheck = getOutTrafficCheck(rootJsonObject);
         allCourseCase.setOutTrafficCheck(outTrafficCheck);
 
         // 결과 리스트 확장 노드
-        // path.length() == busCount
-        JSONArray path = rootJsonObject.getJSONArray("path");
+        JSONArray path = getPath(rootJsonObject);
 
-        // @TODO("루프별로 각 객체들을 포함하는 List 반환")
+        addCourse(courses, path);
+
+        allCourseCase.setCourses(courses);
+
+        return Optional.of(allCourseCase);
+    }
+
+    private static JSONArray getPath(JSONObject rootJsonObject) {
+        JSONArray path = rootJsonObject.getJSONArray("path");
+        return path;
+    }
+
+    private static JSONObject getJsonObject(String jsonResult) {
+        JSONObject jsonObj = new JSONObject(jsonResult);
+        return jsonObj.getJSONObject("result");
+    }
+
+    private static int getSearchType(JSONObject rootJsonObject) {
+        int searchType = rootJsonObject.getInt("searchType");
+        log.debug("searchType = {}", searchType);
+        return searchType;
+    }
+
+    private static int getOutTrafficCheck(JSONObject rootJsonObject) {
+        int outTrafficCheck = rootJsonObject.getInt("outTrafficCheck");
+        log.debug("outTrafficCheck = {}", outTrafficCheck);
+        return outTrafficCheck;
+    }
+
+    private void addCourse(List<Course> courses, JSONArray path) {
         for (int i = 0; i < path.length(); i++) {
 
             Course course = new Course();
@@ -171,124 +193,176 @@ public class MemberService {
             List<SubCourse> subCourses = new ArrayList<>();
 
             // 출발지 -> 목적지 경우의 수 중에서 하나
-            JSONObject pathObj = path.getJSONObject(i);
+            JSONObject pathObj = getPathObj(path, i);
 
             // 요약 정보 확장 노드
-            JSONObject info = pathObj.getJSONObject("info");
+            JSONObject info = getInfo(pathObj);
 
             // 총 요금
-            int payment = info.getInt("payment");
-            log.debug("payment = {}", payment);
+            int payment = getPayment(info);
             course.setPayment(payment);
 
             // 버스 환승 카운트 (1부터 시작)
             // busTransitCount + 1 = 도보
-            int busTransitCount = info.getInt("busTransitCount");
-            log.debug("busTransitCount = {}", busTransitCount);
+            int busTransitCount = getBusTransitCount(info);
             course.setBusTransitCount(busTransitCount);
 
-            // 최초 정류장
-            String firstStartStation = info.getString("firstStartStation");
-            log.debug("firstStartStation = {}", firstStartStation);
-
-            // 최종 정류장
-            String lastEndStation = info.getString("lastEndStation");
-            log.debug("lastEndStation = {}", lastEndStation);
-
             // 이동 교통 수단 정보 확장 노드
-            JSONArray subPath = pathObj.getJSONArray("subPath");
+            JSONArray subPath = getSubPath(pathObj);
 
-            // @TODO("루프별로 각 객체들을 포함하는 List 반환")
-            for (int k = 0; k < subPath.length(); k++) {
-
-                SubCourse subCourse = new SubCourse();
-
-                JSONObject subPathObj = subPath.getJSONObject(k);
-
-                // 이동 수단 종류
-                // 1: 지하철, 2: 버스, 3: 도보 (2, 3인 경우로 구분해서 구현하기)
-                int trafficType = subPathObj.getInt("trafficType");
-                log.debug("trafficType = {}", trafficType);
-                subCourse.setTrafficType(trafficType);
-
-                // 이동 소요 시간
-                int sectionTime = subPathObj.getInt("sectionTime");
-                log.debug("sectionTime = {}", sectionTime);
-                subCourse.setSectionTime(sectionTime);
-
-                if (trafficType == 2) {
-
-                    List<String> busNoList = new ArrayList<>();
-
-                    // 교통 수단 정보 확장 노드
-                    JSONArray lane = subPathObj.getJSONArray("lane");
-
-                    // @TODO("루프별로 각 객체들을 포함하는 List 반환")
-                    for (int r = 0; r < lane.length(); r++) {
-
-                        JSONObject laneObj = lane.getJSONObject(r);
-
-                        // 버스 번호
-                        String busNo = laneObj.getString("busNo");
-                        log.debug("busNo = {}", busNo);
-
-                        busNoList.add(busNo);
-                    }
-
-                    // 승차 정류장
-                    String startName = subPathObj.getString("startName");
-                    log.debug("startName = {}", startName);
-
-                    // 하차 정류장
-                    String endName = subPathObj.getString("endName");
-                    log.debug("endName = {}", endName);
-
-                    // 경로 상세구간 정보 확장 노드
-                    JSONObject passStopList = subPathObj.getJSONObject("passStopList");
-
-                    // 정류장 정보 그룹 노드
-                    JSONArray stations = passStopList.getJSONArray("stations");
-
-                    // 첫번째 정류소 정보
-                    JSONObject firstPassStopObj = stations.getJSONObject(0);
-                    log.debug("firstPassStopObj = {}", firstPassStopObj);
-
-                    // 첫번째 정류장 ID
-                    int firstStationID = firstPassStopObj.getInt("stationID");
-                    log.debug("firstStationID = {}", firstStationID);
-
-                    // 첫번째 정류장 명칭
-                    String firstStationName = firstPassStopObj.getString("stationName");
-                    log.debug("firstStationName = {}", firstStationName);
-
-                    // 마지막 정류소 정보
-                    JSONObject lastPassStopObj = stations.getJSONObject(stations.length() - 1);
-                    log.debug("lastPassStopObj = {}", lastPassStopObj);
-
-                    // 마지막 정류장 ID
-                    int lastStationID = lastPassStopObj.getInt("stationID");
-                    log.debug("lastStationID = {}", lastStationID);
-
-                    // 마지막 정류장 명칭
-                    String lastStationName = lastPassStopObj.getString("stationName");
-                    log.debug("lastStationName = {}", lastStationName);
-
-                    BusLane busLane = new BusLane(busNoList, firstStationID, firstStationName, lastStationID, lastStationName);
-                    subCourse.setBusLane(Optional.of(busLane));
-                }
-
-                subCourses.add(subCourse);
-            }
+            addSubCourse(subCourses, subPath);
 
             course.setSubCourses(subCourses);
             courses.add(course);
         }
-
-        allCourseCase.setCourses(courses);
-        return Optional.of(allCourseCase);
     }
 
-    private static boolean isNotInCity(int searchType) {
+    private static JSONObject getPathObj(JSONArray path, int i) {
+        return path.getJSONObject(i);
+    }
+
+    private static JSONObject getInfo(JSONObject pathObj) {
+        return pathObj.getJSONObject("info");
+    }
+
+    private static int getBusTransitCount(JSONObject info) {
+        int busTransitCount = info.getInt("busTransitCount");
+        log.debug("busTransitCount = {}", busTransitCount);
+        return busTransitCount;
+    }
+
+    private static int getPayment(JSONObject info) {
+        int payment = info.getInt("payment");
+        log.debug("payment = {}", payment);
+        return payment;
+    }
+
+    private static JSONArray getSubPath(JSONObject pathObj) {
+        return pathObj.getJSONArray("subPath");
+    }
+
+    private void addSubCourse(List<SubCourse> subCourses, JSONArray subPath) {
+        for (int k = 0; k < subPath.length(); k++) {
+
+            SubCourse subCourse = new SubCourse();
+
+            JSONObject subPathObj = getSubPathObj(subPath, k);
+
+            // 이동 수단 종류
+            // 1: 지하철, 2: 버스, 3: 도보 (2, 3인 경우로 구분해서 구현하기)
+            int trafficType = getTrafficType(subPathObj);
+            subCourse.setTrafficType(trafficType);
+
+            // 이동 소요 시간
+            int sectionTime = getSectionTime(subPathObj);
+            subCourse.setSectionTime(sectionTime);
+
+            ifPresentBusLane(subCourse, subPathObj, trafficType);
+
+            subCourses.add(subCourse);
+        }
+    }
+
+    private static JSONObject getSubPathObj(JSONArray subPath, int k) {
+        return subPath.getJSONObject(k);
+    }
+
+    private int getTrafficType(JSONObject subPathObj) {
+        int trafficType = subPathObj.getInt("trafficType");
+        log.debug("trafficType = {}", trafficType);
+        return trafficType;
+    }
+
+    private int getSectionTime(JSONObject subPathObj) {
+        int sectionTime = subPathObj.getInt("sectionTime");
+        log.debug("sectionTime = {}", sectionTime);
+        return sectionTime;
+    }
+
+    private void ifPresentBusLane(SubCourse subCourse, JSONObject subPathObj, int trafficType) {
+        if (trafficType == 2) {
+
+            List<String> busNoList = new ArrayList<>();
+
+            // 교통 수단 정보 확장 노드
+            addBusNo(subPathObj, busNoList);
+
+            // 경로 상세구간 정보 확장 노드
+            JSONObject passStopList = getPassStopList(subPathObj);
+
+            // 정류장 정보 그룹 노드
+            JSONArray stations = getStations(passStopList);
+
+            // 첫번째 정류소 정보
+            JSONObject firstPassStopObj = getPassStopObj(stations, 0);
+
+            // 첫번째 정류장 ID
+            int firstStationID = getStationID(firstPassStopObj);
+
+            // 첫번째 정류장 명칭
+            String firstStationName = getStationName(firstPassStopObj);
+
+            // 마지막 정류소 정보
+            JSONObject lastPassStopObj = getPassStopObj(stations, stations.length() - 1);
+
+            // 마지막 정류장 ID
+            int lastStationID = getStationID(lastPassStopObj);
+
+            // 마지막 정류장 명칭
+            String lastStationName = getStationName(lastPassStopObj);
+
+            BusLane busLane = BusLane.createBusLane(busNoList, firstStationID, firstStationName, lastStationID, lastStationName);
+            subCourse.setBusLane(Optional.of(busLane));
+        }
+    }
+
+    private void addBusNo(JSONObject subPathObj, List<String> busNoList) {
+        JSONArray lane = getLane(subPathObj);
+
+        for (int r = 0; r < lane.length(); r++) {
+            busNoList.add(getBusNo(getLaneObj(lane, r)));
+        }
+    }
+
+    private JSONObject getLaneObj(JSONArray lane, int r) {
+        return lane.getJSONObject(r);
+    }
+
+    private JSONArray getLane(JSONObject subPathObj) {
+        return subPathObj.getJSONArray("lane");
+    }
+
+    private String getBusNo(JSONObject laneObj) {
+        String busNo = laneObj.getString("busNo");
+        log.debug("busNo = {}", busNo);
+        return busNo;
+    }
+
+    private JSONObject getPassStopList(JSONObject subPathObj) {
+        return subPathObj.getJSONObject("passStopList");
+    }
+
+    private JSONArray getStations(JSONObject passStopList) {
+        return passStopList.getJSONArray("stations");
+    }
+
+    private JSONObject getPassStopObj(JSONArray stations, int index) {
+        return stations.getJSONObject(index);
+    }
+
+    private String getStationName(JSONObject passStopObj) {
+        String stationName = passStopObj.getString("stationName");
+        log.debug("stationName = {}", stationName);
+        return stationName;
+    }
+
+    private int getStationID(JSONObject firstPassStopObj) {
+        int firstStationID = firstPassStopObj.getInt("stationID");
+        log.debug("StationID = {}", firstStationID);
+        return firstStationID;
+    }
+
+    private boolean isNotInCity(int searchType) {
         return searchType != 0;
     }
 
@@ -330,63 +404,23 @@ public class MemberService {
 
         StringBuilder urlBuilder = new StringBuilder(apiUrl);
 
-        urlBuilder.append("?" + URLEncoder.encode("apiKey", UTF_8) + "=" + apiKey);
+        urlBuilder.append("?").append(URLEncoder.encode("apiKey", UTF_8)).append("=").append(apiKey);
 
         // 출발지 X좌표 (경도좌표)
-        urlBuilder.append("&" + URLEncoder.encode("SX", UTF_8) + "="
-                + URLEncoder.encode(requestDto.getSx(), UTF_8));
+        urlBuilder.append("&").append(URLEncoder.encode("SX", UTF_8)).append("=").append(URLEncoder.encode(requestDto.getSx(), UTF_8));
 
         //출발지 Y좌표 (위도좌표)
-        urlBuilder.append("&" + URLEncoder.encode("SY", UTF_8) + "="
-                + URLEncoder.encode(requestDto.getSy(), UTF_8));
+        urlBuilder.append("&").append(URLEncoder.encode("SY", UTF_8)).append("=").append(URLEncoder.encode(requestDto.getSy(), UTF_8));
 
         // 도착지 X좌표 (경도좌표)
-        urlBuilder.append("&" + URLEncoder.encode("EX", UTF_8) + "="
-                + URLEncoder.encode(requestDto.getEx(), UTF_8));
+        urlBuilder.append("&").append(URLEncoder.encode("EX", UTF_8)).append("=").append(URLEncoder.encode(requestDto.getEx(), UTF_8));
 
         // 도착지 Y좌표 (위도좌표)
-        urlBuilder.append("&" + URLEncoder.encode("EY", UTF_8) + "="
-                + URLEncoder.encode(requestDto.getEy(), UTF_8));
+        urlBuilder.append("&").append(URLEncoder.encode("EY", UTF_8)).append("=").append(URLEncoder.encode(requestDto.getEy(), UTF_8));
 
         // 도시 내 경로수단을 지정한다. (2: 버스)
-        urlBuilder.append("&" + URLEncoder.encode("SearchPathType", UTF_8) + "=" + 2);
+        urlBuilder.append("&").append(URLEncoder.encode("SearchPathType", UTF_8)).append("=").append(2);
 
         return urlBuilder;
     }
-
-    /*@Data
-    static class AllRouteCase {
-        private int outTrafficCheck; // 직통 유무 (0: 직통 o, 1: 직통 x)
-        private List<Path> paths; // 모든 경우의 수
-    }*/
-
-    // 경우의 수 중 하나
-    /*@Data
-    static class Path {
-        private int busTransitCount; // 버스 환승 카운트
-        private int payment; // 요금
-
-        // subPaths.length() == 2 * busTransicCount + 1
-        private List<SubPath> subPaths;
-    }*/
-
-    /*@Data
-    static class SubPath {
-        private List<Lane> lanes;
-    }*/
-
-    /*@Data
-    static class Lane {
-        private int trafficType; // 이동 수단 종류 (1-지하철, 2-버스, 3-도보)
-        private int sectionCount;
-        private BusLane busLane; // trafficType == 2인 경우
-    }*/
-
-    /*@Data
-    static class BusLane {
-        private String busNo; // 버스 번호 (2인 경우)
-        private int boardStationID; // 승차 정류장 ID
-        private String boardStationName; // 승차 정류장 명칭
-        private int alightStationID; // 하차 정류장 ID
-    }*/
 }
