94933cb10528759f4b80da74c7b7f7be62ea58c9
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-01-20 16:27:43 +0900

    refactor(server): ReservationRepository findByBus_Id 메서드 제거

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/reservation/repository/ReservationRepository.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/reservation/repository/ReservationRepository.java
@@ -3,10 +3,7 @@ package kr.ac.kumoh.sjjpl138.wheere.reservation.repository;
 import kr.ac.kumoh.sjjpl138.wheere.reservation.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
-    List<Reservation> findByBus_Id(Long budId);
     Reservation findResvById(Long id);
 }
