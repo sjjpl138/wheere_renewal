5fcf9da1bed33473ccd79d7112cf2d5edfe44b72
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-01-21 00:16:10 +0900

    feat(server): StationRepository findStationByStationIds 메서드 생성

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/station/repository/StationRepository.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/station/repository/StationRepository.java
@@ -2,6 +2,13 @@ package kr.ac.kumoh.sjjpl138.wheere.station.repository;
 
 import kr.ac.kumoh.sjjpl138.wheere.station.Station;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface StationRepository extends JpaRepository<Station, Long> {
+
+    @Query("select s from Station s where s.id in :stationIds")
+    List<Station> findStationByStationIds(@Param("stationIds") List<Long> stationIds);
 }
