f10446b97d67e4b3c36a5ab1ffe9781ac3f33138
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-01-20 15:13:52 +0900

    feat(server): Transfer 생성
    예약-버스(N:N) 중간 테이블 Transfer 생성

---
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/transfer/Transfer.java
@@ -0,0 +1,30 @@
+package kr.ac.kumoh.sjjpl138.wheere.transfer;
+
+import kr.ac.kumoh.sjjpl138.wheere.bus.Bus;
+import kr.ac.kumoh.sjjpl138.wheere.reservation.Reservation;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Transfer {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "TRANSFER_ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "RESERVATION_ID")
+    private Reservation reservation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "BUS_ID")
+    private Bus bus;
+}
