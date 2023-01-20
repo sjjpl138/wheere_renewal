5839cde5a570f237c3f509976f5dcd2c34deb42b
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-01-20 19:13:29 +0900

    feat(server): BusRepository 메서드 추가

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/bus/repository/BusRepository.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/bus/repository/BusRepository.java
@@ -4,10 +4,13 @@ import kr.ac.kumoh.sjjpl138.wheere.bus.Bus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface BusRepository extends JpaRepository<Bus, Long> {
     Optional<Bus> findBusByVehicleNoAndBusNoAndBusDate(String vehicleNo, String busNo, LocalDate operationDate);
 
     Optional<Bus> findBusByVehicleNoAndBusDate(String VehicleNo, LocalDate busDate);
+
+    List<Bus> findByBusNo(String busNo);
 }
