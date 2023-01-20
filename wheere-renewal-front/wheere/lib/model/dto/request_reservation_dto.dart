e873f49594125ffed4f90ca1b6c9ed473d3bb00e
Journeybongbong <77794756+Journeybongbong@users.noreply.github.com>
2023-01-21 00:59:27 +0900

    feature: (user) 예약조회 json 수정

--- wheere-renewal-front/wheere/lib/model/dto/request_reservation_dto.dart
+++ wheere-renewal-front/wheere/lib/model/dto/request_reservation_dto.dart
@@ -1,23 +1,30 @@
+import 'request_reservation_bus_dto.dart';
+
 class RequestReservationDTO {
   String mId;
   int bId;
-  int sStationId;
-  int eStationId;
+  List<RequestReservationBusDTO> buses;
+  String rState;
+  String rPrice;
   String rDate;
 
   RequestReservationDTO({
     required this.mId,
     required this.bId,
-    required this.sStationId,
-    required this.eStationId,
+    required this.buses,
+    required this.rState,
+    required this.rPrice,
     required this.rDate
   });
 
-  Map<String, dynamic> toJson() => {
-    'mId': mId,
-    'bId': bId,
-    'sStationId': sStationId,
-    'eStationId': eStationId,
-    'rDate': rDate
-  };
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    List<Map<String, dynamic>> buses = this.buses.map((i) => i.toJson()).toList();
+    data['mId'] = mId;
+    data['buses'] = buses;
+    data['rState'] = rState;
+    data['rPrice'] = rPrice;
+    data['rDate'] = rDate;
+    return data;
+  }
 }
