315e78ccb6f0ee997925334fd91c725b8b4a4826
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-01-20 19:46:33 +0900

    feat(server): Created RetrieveRoutesRequest

---
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/member/RetrieveRoutesRequest.java
@@ -0,0 +1,21 @@
+package kr.ac.kumoh.sjjpl138.wheere.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RetrieveRoutesRequest {
+
+    private String sx;
+    private String sy;
+    private String ex;
+    private String ey;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate rDate;
+}
