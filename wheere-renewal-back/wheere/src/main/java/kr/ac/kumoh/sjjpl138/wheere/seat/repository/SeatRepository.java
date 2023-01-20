8f4e14150ee2cd9ec59818f430a44019acf3cf89
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-01-20 19:40:28 +0900

    feat(server): Created SeatRepositoryCustom

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/seat/repository/SeatRepository.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/seat/repository/SeatRepository.java
@@ -2,6 +2,12 @@ package kr.ac.kumoh.sjjpl138.wheere.seat.repository;
 
 import kr.ac.kumoh.sjjpl138.wheere.seat.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface SeatRepository extends JpaRepository<Seat, Long> {
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface SeatRepository extends JpaRepository<Seat, Long>, SeatRepositoryCustom {
 }
