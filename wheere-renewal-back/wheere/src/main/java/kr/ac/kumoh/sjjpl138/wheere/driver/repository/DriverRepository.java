3083c906cc8d8abb3e1c9867c7d9b71a0b08cbf6
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-01-20 13:53:41 +0900

    refactor(server): DriverRepository findByBusId 메서드 Optional 반환

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/driver/repository/DriverRepository.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/driver/repository/DriverRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface DriverRepository extends JpaRepository<Driver, String> {
     @Query("select d from Driver d join d.bus b on b.id = :busId")
-    Driver findByBusId(@Param("busId")Long busId);
+    Optional<Driver> findByBusId(@Param("busId")Long busId);
 }
