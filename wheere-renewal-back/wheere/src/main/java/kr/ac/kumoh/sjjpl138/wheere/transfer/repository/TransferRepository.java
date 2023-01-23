acaf8d29a6691d605ab998ea4e2b995bd57da299
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-01-23 21:04:04 +0900

    feat(server): TransferRepository 생성

---
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/transfer/repository/TransferRepository.java
@@ -0,0 +1,7 @@
+package kr.ac.kumoh.sjjpl138.wheere.transfer.repository;
+
+import kr.ac.kumoh.sjjpl138.wheere.transfer.Transfer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransferRepository extends JpaRepository<Transfer, Long> {
+}
