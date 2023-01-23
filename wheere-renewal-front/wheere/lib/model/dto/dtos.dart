25fd105b1b8cbbee79efe64d15eb49b0f06542c9
Journeybongbong <77794756+Journeybongbong@users.noreply.github.com>
2023-01-24 00:03:15 +0900

    feature: (driver) 실시간 버스 위치 dto, repo 구현

--- wheere-renewal-front/wheere/lib/model/dto/dtos.dart
+++ wheere-renewal-front/wheere/lib/model/dto/dtos.dart
@@ -15,3 +15,5 @@ export 'alarm_list_dto.dart';
 export 'member_info_dto.dart';
 export 'firebase_sign_up_dto.dart';
 export 'has_new_alarm_dto.dart';
+export 'bus_location_dto.dart';
+export 'request_busLocation_dto.dart';
