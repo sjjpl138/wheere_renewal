c6a17ed116e3d02ed05d00f55ac3c89a0f9ef423
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-01-23 15:43:22 +0900

    feat(server): SeatRepository findSeatByBIdAndDate 추가

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/seat/repository/SeatRepository.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/seat/repository/SeatRepository.java
@@ -7,7 +7,10 @@ import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
+
 
 public interface SeatRepository extends JpaRepository<Seat, Long>, SeatRepositoryCustom {
+
+    @Query("select s from Seat s join s.platform p on p.bus.id = :busId and p.stationSeq in :seqList where s.seatDate = :resvDate")
+    List<Seat> findSeatByBIdAndDate(@Param("busId")Long busId, @Param("resvDate") LocalDate resvDate, @Param("seqList") List<Integer> seqList);
 }
