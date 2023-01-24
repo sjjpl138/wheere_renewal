cbfc4eb6d4196bb3fbaac6c782426a51686fca82
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-01-24 15:40:30 +0900

    feat(server): StationService 버스 정류장 조회

---
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/station/service/StationService.java
@@ -0,0 +1,25 @@
+package kr.ac.kumoh.sjjpl138.wheere.station.service;
+
+import kr.ac.kumoh.sjjpl138.wheere.station.Station;
+import kr.ac.kumoh.sjjpl138.wheere.station.repository.StationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class StationService {
+    private final StationRepository stationRepository;
+
+    /**
+     * 버스 정류장 조회
+     * @param bId
+     * @return
+     */
+    public List<Station> findStationList(Long bId) {
+        return stationRepository.findStationByBusId(bId);
+    }
+}
