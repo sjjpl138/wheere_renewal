fbd3d8209b0ac0ed4eef9990be4344d994e6423a
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-01-24 11:55:58 +0900

    feat(server): ReservationRepository findByTransferId 추가

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/reservation/repository/ReservationRepository.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/reservation/repository/ReservationRepository.java
@@ -2,8 +2,15 @@ package kr.ac.kumoh.sjjpl138.wheere.reservation.repository;
 
 import kr.ac.kumoh.sjjpl138.wheere.reservation.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     Reservation findResvById(Long id);
+
+    @Query("select r from Reservation r join fetch Transfer as t on t.reservation.id = r.id")
+    List<Reservation> findByTransferId(@Param("transferId") Long transferId);
 }
