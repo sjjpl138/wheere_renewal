2c89047ce2fbb78e2f3588e6537f058ae2dfa420
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-01-24 22:18:38 +0900

    feat(server): ReservationApiController PageableDefault 추가

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/reservation/api/ReservationApiController.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/reservation/api/ReservationApiController.java
@@ -7,6 +7,8 @@ import kr.ac.kumoh.sjjpl138.wheere.reservation.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,11 +24,18 @@ public class ReservationApiController {
     private final ReservationService reservationService;
     private final ReservationRepository reservationRepository;
 
+    /**
+     * 예약 조회
+     * @param mId 사용자ID (PK)
+     * @param condition 검색 조건 (예약 상태)
+     * @param pageable (size, page, sort)
+     * @return ReservationListDto
+     */
     @GetMapping("/{mId}")
     public ResponseEntity<Slice<ReservationListDto>> reservationList(
             @PathVariable("mId") String mId,
             ReservationSearchCondition condition,
-            Pageable pageable) {
+            @PageableDefault(size=10, sort="reservationDate", direction = Sort.Direction.DESC) Pageable pageable) {
 
         Slice<ReservationListDto> reservations = reservationRepository.searchSlice(mId, condition, pageable);
 
