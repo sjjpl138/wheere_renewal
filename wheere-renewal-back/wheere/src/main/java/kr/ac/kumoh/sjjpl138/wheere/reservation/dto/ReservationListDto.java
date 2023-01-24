9555608bba44897cccfe52586655d82d4d785077
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-01-24 22:01:08 +0900

    refactor(server): ReservationListDto 애노테이션 추가

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/reservation/dto/ReservationListDto.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/reservation/dto/ReservationListDto.java
@@ -1,5 +1,6 @@
 package kr.ac.kumoh.sjjpl138.wheere.reservation.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import kr.ac.kumoh.sjjpl138.wheere.reservation.ReservationStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,7 +12,11 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ReservationListDto {
+
+    @JsonProperty("rId")
     private Long rId;
+
+    @JsonProperty("rDate")
     private LocalDate rDate;
     //        private String bNo;
 //        private Long routeId;
@@ -22,5 +27,7 @@ public class ReservationListDto {
 //        private LocalTime eTime;
 //        private Long eStationId;
 //        private String eStationName;
+
+    @JsonProperty("rState")
     private ReservationStatus rState;
 }
