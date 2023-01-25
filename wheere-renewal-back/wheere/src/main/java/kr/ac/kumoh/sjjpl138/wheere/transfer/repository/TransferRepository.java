c2bdfa4d367c069dbb6d39457d64d9bcffa8b004
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-01-25 18:30:49 +0900

    faet(server): TransferRepository findTransferByBusId 메서드 추가

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/transfer/repository/TransferRepository.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/transfer/repository/TransferRepository.java
@@ -1,7 +1,9 @@
 package kr.ac.kumoh.sjjpl138.wheere.transfer.repository;
 
 import kr.ac.kumoh.sjjpl138.wheere.transfer.Transfer;
+import kr.ac.kumoh.sjjpl138.wheere.transfer.dto.TransferDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
@@ -12,4 +14,7 @@ public interface TransferRepository extends JpaRepository<Transfer, Long> {
     List<Transfer> findByReservation_Member_IdAndBus_IdAndReservation_ReservationDate(String memberId, Long busId, LocalDate resvDate);
 
     List<Transfer> findByBus_IdAndReservation_Id(@Param("busId") Long busId, @Param("resvId") Long resvId);
+
+    @Query("select new kr.ac.kumoh.sjjpl138.wheere.transfer.dto.TransferDto(r.id, r.reservationStatus, t.boardStation, t.alightStation) from Transfer t join t.bus b on b.id = :busId join t.reservation r")
+    List<TransferDto> findTransferByBusId(@Param("busId")Long bId);
 }
