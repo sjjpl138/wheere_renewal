02b58ec3caaabe62f71cc376c29dce53eb1eef11
Journeybongbong <77794756+Journeybongbong@users.noreply.github.com>
2023-01-26 00:44:23 +0900

    feature: (driver) baseRemoteDataSource 수정

--- wheere-renewal-front/wheere_driver/lib/model/repository/request_bus_location_repository.dart
+++ wheere-renewal-front/wheere_driver/lib/model/repository/request_bus_location_repository.dart
@@ -1,5 +1,5 @@
 import 'remote_data_sources/remote_data_sources.dart';
-import 'package:wheere/model/dto/dtos.dart';
+import 'package:wheere_driver/model/dto/dtos.dart';
 
 class RequestBusLocationRepository {
   final RequestBusLocationDataSource _requestBusLocationDataSource = RequestBusLocationDataSource();
