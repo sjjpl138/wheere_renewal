bd21423a609cf31b27006100ca417c2e47526c30
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-01-26 20:04:44 +0900

    refactor(server): TransferRepository findByBus_IdAndReservation_Id 파라미터 수정

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/transfer/repository/TransferRepository.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/transfer/repository/TransferRepository.java
@@ -13,7 +13,7 @@ public interface TransferRepository extends JpaRepository<Transfer, Long> {
 
     List<Transfer> findByReservation_Member_IdAndBus_IdAndReservation_ReservationDate(String memberId, Long busId, LocalDate resvDate);
 
-    List<Transfer> findByBus_IdAndReservation_Id(@Param("busId") Long busId, @Param("resvId") Long resvId);
+    List<Transfer> findByBus_IdAndReservation_Id(Long busId, Long resvId);
 
     @Query("select new kr.ac.kumoh.sjjpl138.wheere.transfer.dto.TransferDto(r.id, r.reservationStatus, t.boardStation, t.alightStation) from Transfer t join t.bus b on b.id = :busId join t.reservation r")
     List<TransferDto> findTransferByBusId(@Param("busId")Long bId);
