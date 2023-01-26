898ce3ea11bf1d380d72f71a3e56138d4d8f2916
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-01-27 00:57:43 +0900

    refactor(server): ReservationSearchCondition 패키지 변경

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/reservation/repository/ReservationCustom.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/reservation/repository/ReservationCustom.java
@@ -1,7 +1,7 @@
 package kr.ac.kumoh.sjjpl138.wheere.reservation.repository;
 
 import kr.ac.kumoh.sjjpl138.wheere.reservation.Reservation;
-import kr.ac.kumoh.sjjpl138.wheere.reservation.ReservationSearchCondition;
+import kr.ac.kumoh.sjjpl138.wheere.reservation.request.ReservationSearchCondition;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
