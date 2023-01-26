88428f2a20be86244c7c82b3794958274bdcd37c
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-01-27 00:52:32 +0900

    feat(server): Created findTransferByReservation 메서드

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/transfer/repository/TransferRepository.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/transfer/repository/TransferRepository.java
@@ -1,7 +1,9 @@
 package kr.ac.kumoh.sjjpl138.wheere.transfer.repository;
 
+import kr.ac.kumoh.sjjpl138.wheere.reservation.Reservation;
 import kr.ac.kumoh.sjjpl138.wheere.transfer.Transfer;
 import kr.ac.kumoh.sjjpl138.wheere.transfer.dto.TransferDto;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,4 +19,7 @@ public interface TransferRepository extends JpaRepository<Transfer, Long> {
 
     @Query("select new kr.ac.kumoh.sjjpl138.wheere.transfer.dto.TransferDto(r.id, r.reservationStatus, t.boardStation, t.alightStation) from Transfer t join t.bus b on b.id = :busId join t.reservation r")
     List<TransferDto> findTransferByBusId(@Param("busId")Long bId);
+
+    @EntityGraph(attributePaths = {"bus"})
+    List<Transfer> findByReservation(Reservation reservation);
 }
