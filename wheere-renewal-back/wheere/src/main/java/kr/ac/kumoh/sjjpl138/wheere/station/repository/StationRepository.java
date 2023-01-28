34cae1029310b668c00d9d95063945fc69a53dd0
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-01-29 01:49:18 +0900

    refactor(server): StationRepository 메서드 수정

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/station/repository/StationRepository.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/station/repository/StationRepository.java
@@ -2,22 +2,12 @@ package kr.ac.kumoh.sjjpl138.wheere.station.repository;
 
 import kr.ac.kumoh.sjjpl138.wheere.station.Station;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface StationRepository extends JpaRepository<Station, Long> {
 
-    @Query("select s from Station s where s.id in :stationIds")
-    List<Station> findStationByStationIds(@Param("stationIds") List<Long> stationIds);
+    List<Station> findStationByIdIn(List<Long> stationIds);
 
-    @Query("select s from Platform p join p.station s where  p.id in :platformId")
-    List<Station> findStationByPlatformId(@Param("platformId") List<Long> platformIds);
-
-    @Query("select s from Platform p join p.station s join p.bus b where b.id = :busId")
-    List<Station> findStationByBusId(@Param("busId") Long busId);
-
-    @Query("select s from Station s where s.name in :sNames")
-    List<Station> findStationByNameIn(@Param("sNames") List<String> stationNames);
+    List<Station> findStationByNameIn(List<String> stationNames);
 }
