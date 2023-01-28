6a6367801e74b87a338f44220517a960787ee0ef
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-01-29 00:22:42 +0900

    refactor(server): StationService findStationByPlatformAndBus 삭제

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/station/service/StationService.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/station/service/StationService.java
@@ -1,14 +1,11 @@
 package kr.ac.kumoh.sjjpl138.wheere.station.service;
 
-import kr.ac.kumoh.sjjpl138.wheere.platform.Platform;
-import kr.ac.kumoh.sjjpl138.wheere.platform.dto.StationInfo;
 import kr.ac.kumoh.sjjpl138.wheere.station.Station;
 import kr.ac.kumoh.sjjpl138.wheere.station.repository.StationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,16 +22,5 @@ public class StationService {
     public List<Station> findStationList(Long bId) {
         return stationRepository.findStationByBusId(bId);
     }
-
-    public List<StationInfo> findStationByPlatformAndBus(List<Long> stationIds) {
-        List<Platform> platforms = stationRepository.findStationByPlatformAndBus(stationIds);
         List<StationInfo> infoList = new ArrayList<>();
-        for (Platform platform : platforms) {
-            Station station = platform.getStation();
-            StationInfo info = new StationInfo(station.getName(), platform.getArrivalTime());
-            infoList.add(info);
-        }
-
-        return infoList;
-    }
 }
