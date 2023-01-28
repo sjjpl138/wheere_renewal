29b8bff41296c6e732cc88b292f7f280adf0923f
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-01-28 23:08:08 +0900

    refactor(server): StationRepository 수정

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/station/repository/StationRepository.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/station/repository/StationRepository.java
@@ -1,5 +1,6 @@
 package kr.ac.kumoh.sjjpl138.wheere.station.repository;
 
+import kr.ac.kumoh.sjjpl138.wheere.platform.Platform;
 import kr.ac.kumoh.sjjpl138.wheere.platform.dto.StationInfo;
 import kr.ac.kumoh.sjjpl138.wheere.station.Station;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -22,6 +23,6 @@ public interface StationRepository extends JpaRepository<Station, Long> {
     @Query("select s.id from Station s where s.name in :sNames")
     List<Long> findStationByNames(@Param("sNames") List<String> sNames);
 
-    @Query("select new kr.ac.kumoh.sjjpl138.wheere.platform.dto.StationInfo(s.name, p.arrivalTime) from Platform p join p.station s on s.id in :stationId")
-    List<StationInfo> findStationByPlatformAndBus(@Param("stationId") List<Long> stationId);
+    @Query("select p from Platform p join p.station s on s.id in :stationId")
+    List<Platform> findStationByPlatformAndBus(@Param("stationId") List<Long> stationId);
 }
