6dbd146f730b264201590391446efd460b7a9178
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-01-31 19:11:14 +0900

    feat(server): ReservationRepository - findReservationWithMemberById 메서드 구현

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/reservation/repository/ReservationRepository.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/reservation/repository/ReservationRepository.java
@@ -1,11 +1,13 @@
 package kr.ac.kumoh.sjjpl138.wheere.reservation.repository;
 
 import kr.ac.kumoh.sjjpl138.wheere.reservation.Reservation;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long>, ReservationCustom {
 
@@ -13,4 +15,7 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long>,
 
     @Query("select r from Reservation r join fetch Transfer as t on t.reservation.id = r.id")
     List<Reservation> findByTransferId(@Param("transferId") Long transferId);
+
+    @EntityGraph(attributePaths = {"member"})
+    Optional<Reservation> findReservationWithMemberById(Long id);
 }
