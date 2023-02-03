9f12dd1170b50b627c0887f52a826e455eb52541
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-02-04 02:41:26 +0900

    feat(server): SeatRepository 메서드 추가

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/seat/repository/SeatRepository.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/seat/repository/SeatRepository.java
@@ -5,12 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.swing.text.html.Option;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 
 public interface SeatRepository extends JpaRepository<Seat, Long>, SeatRepositoryCustom {
 
     @Query("select s from Seat s join s.platform p on p.bus.id = :busId and p.stationSeq in :seqList where s.seatDate = :resvDate")
-    List<Seat> findSeatByBIdAndDate(@Param("busId")Long busId, @Param("resvDate") LocalDate resvDate, @Param("seqList") List<Integer> seqList);
+    List<Seat> findSeatByBIdAndDate(@Param("busId") Long busId, @Param("resvDate") LocalDate resvDate, @Param("seqList") List<Integer> seqList);
+
+    @Query("select min(s.leftSeatsNum) from Seat s join s.platform p on p.bus.id = :busId and p.stationSeq in :seqList where s.seatDate = :resvDate")
+    Optional<Integer> findMinLeftSeatsByBusIdAndDateAndSeqIn(@Param("busId") Long busId, @Param("resvDate") LocalDate resvDate, @Param("seqList") List<Integer> seqList);
 }
