48bd1b1c0841b4d47409696f96d35aea7cbdaacc
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-02-04 21:15:34 +0900

    refactor(server): BusRoute JsonProperty 애노테이션 추가

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/member/response/BusRoute.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/member/response/BusRoute.java
@@ -1,5 +1,6 @@
 package kr.ac.kumoh.sjjpl138.wheere.member.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -8,15 +9,23 @@ import java.util.List;
 
 @Data
 public class BusRoute {
+    @JsonProperty("bNo")
     private String bNo;
     private List<Long> busId;
+    @JsonProperty("sStationId")
     private int sStationId;
+    @JsonProperty("sStationName")
     private String sStationName;
+    @JsonProperty("sTime")
     @DateTimeFormat(pattern = "HH:mm:ss")
     private List<LocalTime> sTime;
+    @JsonProperty("eStationId")
     private int eStationId;
+    @JsonProperty("eStationName")
     private String eStationName;
+    @JsonProperty("eTime")
     @DateTimeFormat(pattern = "HH:mm:ss")
     private List<LocalTime> eTime;
+
     private List<Integer> leftSeats;
 }
