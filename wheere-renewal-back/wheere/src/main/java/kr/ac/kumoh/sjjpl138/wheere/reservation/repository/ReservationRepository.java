c225dd3f317ebfad7667932597d155a108b38b98
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-02-13 20:54:13 +0900

    feat(server): ReservationRepository 메서드 추가

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/reservation/repository/ReservationRepository.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/reservation/repository/ReservationRepository.java
@@ -1,11 +1,13 @@
 package kr.ac.kumoh.sjjpl138.wheere.reservation.repository;
 
 import kr.ac.kumoh.sjjpl138.wheere.reservation.Reservation;
+import kr.ac.kumoh.sjjpl138.wheere.reservation.ReservationStatus;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,4 +20,7 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long>,
 
     @EntityGraph(attributePaths = {"member"})
     Optional<Reservation> findReservationWithMemberById(Long id);
+
+    @Query("select r from Reservation r join r.member m where m.id = :mId and r.reservationStatus != :rStatus and r.reservationDate = :rDate")
+    List<Reservation> findCancelReservation(@Param("mId") String mId, @Param("rStatus")ReservationStatus rStatus, @Param("rDate") LocalDate rDate);
 }
