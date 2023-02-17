742c5d6259b6277c5c9b11005c2040ccf6f696a4
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-02-17 17:12:41 +0900

    refactor(server): createReservationBus 생성

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/reservation/response/ReservationBus.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/reservation/response/ReservationBus.java
@@ -1,11 +1,15 @@
 package kr.ac.kumoh.sjjpl138.wheere.reservation.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import kr.ac.kumoh.sjjpl138.wheere.bus.Bus;
+import kr.ac.kumoh.sjjpl138.wheere.platform.Platform;
+import kr.ac.kumoh.sjjpl138.wheere.station.Station;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalTime;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -33,4 +37,29 @@ public class ReservationBus {
     private Long eStationId;
     @JsonProperty("eStationName")
     private String eStationName;
+
+    public static ReservationBus createReservationBus(Bus bus, List<Platform> platforms) {
+
+        ReservationBus reservationBus = new ReservationBus();
+
+        reservationBus.setBId(bus.getId());
+        reservationBus.setBNo(bus.getBusNo());
+        reservationBus.setRouteId(bus.getRouteId());
+        reservationBus.setVNo(bus.getVehicleNo());
+
+        Platform boardPlatform = platforms.get(0);
+        Station findBoardStation = boardPlatform.getStation();
+        Platform alightPlatform = platforms.get(1);
+        Station findAlightStation = alightPlatform.getStation();
+
+        reservationBus.setSTime(boardPlatform.getArrivalTime());
+        reservationBus.setSStationId(findBoardStation.getId());
+        reservationBus.setSStationName(findBoardStation.getName());
+
+        reservationBus.setETime(alightPlatform.getArrivalTime());
+        reservationBus.setEStationId(findAlightStation.getId());
+        reservationBus.setEStationName(findAlightStation.getName());
+
+        return reservationBus;
+    }
 }
