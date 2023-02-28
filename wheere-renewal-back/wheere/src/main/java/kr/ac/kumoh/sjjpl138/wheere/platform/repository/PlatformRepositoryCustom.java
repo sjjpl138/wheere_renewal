70e55ff939171abb30a75ab5f9ba693e41f56445
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-02-28 19:45:46 +0900

    feat(server): PlatformRepositoryCustom findPagedStationsByBusIdAndStationSeq 메서드 추가

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/platform/repository/PlatformRepositoryCustom.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/platform/repository/PlatformRepositoryCustom.java
@@ -1,6 +1,7 @@
 package kr.ac.kumoh.sjjpl138.wheere.platform.repository;
 
-import java.time.LocalTime;
+import kr.ac.kumoh.sjjpl138.wheere.platform.Platform;
+
 import java.util.List;
 
 public interface PlatformRepositoryCustom {
@@ -8,4 +9,6 @@ public interface PlatformRepositoryCustom {
     List<Integer> findAllocationSeqByBusIdAndStationIdList(Long busId, List<Long> stationIds);
 
     List<Integer> findAllocationSeqByBusIdAndStationNameList(Long busId, List<String > stationName);
+
+    List<Platform> findPagedStationsByBusIdAndStationSeq(Long busId, int offset, int limit);
 }
