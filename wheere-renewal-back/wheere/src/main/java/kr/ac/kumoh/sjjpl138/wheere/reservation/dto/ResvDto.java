c35dc95bda0a0c5bd8b2eca68a16c35885581207
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-03-02 16:38:22 +0900

    refactor(server): ResvDto 변수 추가

--- wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/reservation/dto/ResvDto.java
+++ wheere-renewal-back/wheere/src/main/java/kr/ac/kumoh/sjjpl138/wheere/reservation/dto/ResvDto.java
@@ -1,15 +1,19 @@
 package kr.ac.kumoh.sjjpl138.wheere.reservation.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import kr.ac.kumoh.sjjpl138.wheere.member.Member;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 public class ResvDto {
     @JsonProperty("rId")
-    private Long rId;
+    private long rId;
     private int startSeq;
     private int endSeq;
-
+    private long bId;
+    private ResvRequestMemberInfo member;
 }
